986c23ae71721fbf44430fb6ecf14bc50cfd31f9
djx <1442713848@qq.com>
2019-04-27 23:05:46 +0800

    sadasd

---
+++ src/main/java/newClassTwo.java
@@ -0,0 +1,9 @@
+public class newClassTwo {
+    public static void main(String[] args) {
+        System.out.println("asdsad111111111111111");
+        System.out.println("asdsad111111111111111");
+        System.out.println("asdsad111111111111111");
+        System.out.println("asdsad111111111111111");
+        System.out.println("asdsad111111111111111");
+    }
+}
